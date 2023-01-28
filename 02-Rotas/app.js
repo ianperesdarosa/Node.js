0e977217a8224d43ff3b2e24742b1150af8a5183
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-01-28 17:08:54 -0300

    :fire:

--- 02-Rotas/app.js
+++ 02-Rotas/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 
 
+app.get("/", function(req, res) {
+    res.send("Welcome!");
+});
 
-console.log("on");
-app.listen(9001);
+app.get("/perfil", function(req, res) {
+    res.send("Meu perfil");
+});
+
+app.get("/dados", function(req, res) {
+    res.send("Meus dados");
+});
+
+app.listen(9001, function() {
+    console.log("on");
+});
