eff7bd3b585ed039bfb7bb109cadabaacd2cd332
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-01-28 18:07:35 -0300

    :fire:

--- 02-Rotas_com_express/app.js
+++ 02-Rotas_com_express/app.js
@@ -14,6 +14,9 @@ app.get("/dados", function(req, res) {
     res.send("Meus dados");
 });
 
-app.listen(9001, function() {
-    console.log("on");
+app.get("/data/:nome/:cargo/:soldo", function(req, res) {
+    res.send("<h1>Nome: " + req.params.nome + "</h1>" + "<h1> Cargo: " + req.params.cargo + "</h1>"  +  "<h1>Salario: " + req.params.soldo + "</h1>");
+
 });
+
+app.listen(9001, function() {console.log("on");});
