830f09c96d894addcfba4030da9356dabe46f15e
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-06-11 04:46:01 -0300

    :fire:

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './imgs/2B.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 id='texto-zigzag'>Hello!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
