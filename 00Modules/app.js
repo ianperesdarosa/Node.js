9e5713221951247116883e0db880d8700e85eb9f
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-06-11 21:24:10 -0300

    :fire:

---
+++ 00Modules/app.js
@@ -0,0 +1,5 @@
+const media = require('./index');
+
+const res = media.somar(2,2);
+
+console.log(res);
---
+++ 01Http/app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+
+http.createServer((req, res ) =>  {
+  res.write("Aprendendo Node.js!");
+  res.end();  
+}).listen(3020);
